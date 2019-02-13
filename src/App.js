364340b20cd49f97379f0fcf57c0281f0cbff779
Joy <stepjoyous@gmail.com>
2019-02-13 15:48:11 -0800

    Put in boilerplate for App.js and replaced App.css code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+        <div className="App">
+            <header className="App-header">PROJECT 4 ASSESSMENT</header>
+            <main>
+                Your Two Components Go header
+            </main>
+        </div>
+        );
+    }
+}
+
+export default App;
